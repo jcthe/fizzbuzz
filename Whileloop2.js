e9e565ecd930cc3f3ff4d51e13134a7d190199a8
8226 <8226@fresnou.org>
2023-02-23 09:37:49 -0800

    commit i have change some stuff

---
+++ Whileloop2.js
@@ -0,0 +1,29 @@
+const readLine = require('readline-sync');
+let start=
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+let answer = readLine.question("Enter y or n: ");
+
+while (answer != "y" && answer != "n"){
+
+    console.log("invalid choice");
+
+    answer = readLine.question("Enter y or n: ");
+
+}
+
