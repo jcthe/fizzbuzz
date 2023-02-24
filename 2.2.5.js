ec7bc95d9a2f29f637c7160ac06dc4581a331eb4
8226 <8226@fresnou.org>
2023-02-24 10:28:05 -0800

    I have commit

---
+++ 2.2.5.js
@@ -0,0 +1,27 @@
+//for (let i = 0; i <= 150; i++) {
+//        console.log(i);
+//}
+
+
+
+// for (let i = 0; i <= 40; i++){
+   //  console.log(i);
+ //}
+
+ // for (let i = 1; i <= 40; i++){
+ // {
+ //    console.log(i**2);
+ //  }
+ // }
+
+// for (let i = 1; i <= 40; i++){
+//     {
+//         console.log(i**3);
+//     }
+// }
+
+ for (let i = 9; i <= 700; i++){
+     {
+         console.log(i);
+     }
+ }
